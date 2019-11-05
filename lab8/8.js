e0f840e6d35de2142442776e70027c7236f99146
Aolan13 <pakemo@i.ua>
2019-11-05 17:21:09 +0200

    l

--- lab8/8.js
+++ lab8/8.js
@@ -1,62 +1,52 @@
-function nod(obj)
-{
-	var n=obj.num1.value
-	var m=obj.num2.value
-	var p = n%m
-	while (p!=0)
-	{
-		n=m
-		m=p
-		p=n%m
+function nod (obj) {
+	var n = obj.num1.value
+	var m = obj.num2.value
+	var p = n % m
+	while (p != 0) {
+		n = m
+		m = p
+		p = n % m
 	}
-	alert("НСД = "+m);
+	alert('НСД = ' + m)
 }
 
-function reverse(obj)
-{
-	var n=obj.num1.value
-	var r=0;
-	while (n>0)
-	{
-		r=r*10+(n%10);
-		n=Math.trunc(n/10);
+function reverse (obj) {
+	var n = obj.num1.value
+	var r = 0
+	while (n > 0) {
+		r = r * 10 + (n % 10)
+		n = Math.trunc(n / 10)
 	}
-	
-	alert(r);
+
+	alert(r)
 }
 
-function calc()
-{
-	var r=1;
-	for (let i=0; i<999999;i++)
-	{
-		let n=i;
-		let s=0;
-		for (let j=0; j<3; j++)
-		{
-			s+=n%10;
-			n=Math.trunc(n/10);
+function calc () {
+	var r = 1
+	for (let i = 0; i < 999999; i++) {
+		let n = i
+		let s = 0
+		for (let j = 0; j < 3; j++) {
+			s += n % 10
+			n = Math.trunc(n / 10)
 		}
 
-		for (let j=0; j<3; j++)
-		{
-			s-=n%10;
-			n=Math.trunc(n/10);
+		for (let j = 0; j < 3; j++) {
+			s -= n % 10
+			n = Math.trunc(n / 10)
 		}
-		if (s==0) r++;
+		if (s == 0) r++
 	}
 
-	alert(r);
+	alert(r)
 }
 
-function dil(obj)
-{
-	let n = obj.num.value;
-	let s="Дільники заданого натурального числа"+"\n";
-	for (let i=1; i<=n/2; i++)
-	{
-		if ((n%i)==0) s+=i+"\n";
+function dil (obj) {
+	const n = obj.num.value
+	let s = 'Дільники заданого натурального числа' + '\n'
+	for (let i = 1; i <= n / 2; i++) {
+		if ((n % i) == 0) s += i + '\n'
 	}
-	s+=n;
-	obj.data.value=s;
+	s += n
+	obj.data.value = s
 }
